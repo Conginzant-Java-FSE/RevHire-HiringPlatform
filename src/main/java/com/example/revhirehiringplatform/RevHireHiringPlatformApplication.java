3f0b0d18bc6044d9b49a5e3a729741a7ad93bb07
Seemashaik177 <seemashaik177@gmail.com>
2026-02-07 14:23:23 +0530

    First Commit

---
+++ src/main/java/com/example/revhirehiringplatform/RevHireHiringPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.revhirehiringplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevHireHiringPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevHireHiringPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/revhirehiringplatform/RevHireHiringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.revhirehiringplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevHireHiringPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
