fd08702273359ce54abc56dec4dbb5df8d1398e8
avinash <avinashs0527@gmail.com>
2026-02-10 22:30:43 +0530

    added user repositories

---
+++ src/main/java/com/example/revhirehiringplatform/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.revhire.repository;
+
+import com.revhire.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
