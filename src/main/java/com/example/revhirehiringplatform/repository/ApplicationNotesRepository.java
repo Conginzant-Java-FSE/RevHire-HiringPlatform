034de861f94af4de96fe9807594e8a7dbd8f3c1d
SeemaShaik <106534274+SkSeema@users.noreply.github.com>
2026-02-10 19:30:24 +0530

    Delete src/main/java/com/example/revhirehiringplatform/repository/ApplicationNotesRepository.java

--- src/main/java/com/example/revhirehiringplatform/repository/ApplicationNotesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.revhirehiringplatform.repository;
-
-import com.example.revhirehiringplatform.model.ApplicationNotes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApplicationNotesRepository extends JpaRepository<ApplicationNotes, Long> {
-}
