1cfe129cf0055700bb4dfda7c256339eff88bc09
SeemaShaik <106534274+SkSeema@users.noreply.github.com>
2026-02-10 19:33:41 +0530

    Delete src/main/java/com/example/revhirehiringplatform/repository directory

--- src/main/java/com/example/revhirehiringplatform/repository/ApplicationStatusHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.revhirehiringplatform.repository;
-
-import com.example.revhirehiringplatform.model.ApplicationStatusHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApplicationStatusHistoryRepository extends JpaRepository<ApplicationStatusHistory, Long> {
-}
