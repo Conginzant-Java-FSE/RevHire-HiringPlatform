cdb6221a8f1b9d02a846772178462a95efea48f8
Divya Madhuri Konatala <konataladivyamadhuri@gmail.com>
2026-02-12 21:02:59 +0530

    added Auth Response

---
+++ src/main/java/com/example/revhirehiringplatform/dto/response/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.example.revhirehiringplatform.dto.response;
+
+import com.example.revhirehiringplatform.model.User.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String name;
+    private String email;
+    private Role role;
+    private Long id;
+}
